b671daa8e994981583353cff2227fcbc2805dc7a
skypie0102 <skypirate1202@gmail.com>
2025-10-15 22:21:59 +0800

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,30 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "com.example.crtoverlay"
     compileSdk = 34
-
     defaultConfig {
-        applicationId = "com.example.crtoverlay"
+        applicationId = "com.skypie.crtoverlay"
         minSdk = 21
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
-    }
-
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = "17"
     }
 }
 
